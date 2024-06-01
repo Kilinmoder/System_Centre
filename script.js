21c7a9179d31f175111d4e0f67f10c6f0c7df581
enigfrank <xrh3248486103@vip.qq.com>
2024-06-02 03:45:13 +0800

    new file:   img/bg.jpg 	modified:   index.html 	modified:   script.js 	modified:   styles.css

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var ipAddress;
 fetch('https://api.ipify.org?format=json')
   .then(response => response.json())
   .then(data => {
-    ipAddress = "您的IP为: " + data.ip + "  您的IP已被记录!!";
+    ipAddress = "您的IP为:" + data.ip + " | 欢迎访问本网站!";
     document.getElementById('ipAddress').innerHTML = ipAddress;
   })
   .catch(error => console.error('Unable to get IP address:', error));

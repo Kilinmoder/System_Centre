94fd82506e5af29d87f86e989f6eaffc00f06070
enigfrank <xrh3248486103@vip.qq.com>
2024-06-02 00:54:41 +0800

    modified:   index.html 	modified:   script.js 	modified:   styles.css

--- script.js
+++ script.js
@@ -6,3 +6,15 @@ fetch('https://api.ipify.org?format=json')
     document.getElementById('ipAddress').innerHTML = ipAddress;
   })
   .catch(error => console.error('Unable to get IP address:', error));
+
+  function createSakura() {
+    var sakura = document.createElement("div");
+    sakura.className = "sakura";
+    sakura.style.left = Math.random() * 100 + "vw";
+    sakura.style.animationDelay = Math.random() * 5 + "s";
+    document.body.appendChild(sakura);
+
+    setTimeout(function () {
+        sakura.remove();
+    }, 10000);
+}
